b38b1f4262dcc77cb61295a4a3ac6647535975b3
ajwaters061491 <45111585+ajwaters061491@users.noreply.github.com>
2019-08-16 00:08:21 -0400

    added post method, see comments

--- server/routes/index.js
+++ server/routes/index.js
@@ -3,9 +3,31 @@ const   express = require('express'),
         router = express.Router()
         recipes = require('../models/recipeObject').recipe
 
+        mongoclient = require('mongodb').MongoClient //mongoclient reference
+        url = 'I literally cannot tell where the db url is'; //no really, I can't
+
 
 router.get('/',(req, res)=>{
 
+})
+
+//You guys have the structures in a very odd way, 
+//this is more or less the basis of a recipe insert from what I can tell
+router.post('/insert', function(req, res, next) {
+
+    mongoclient.connect(url, function(err, db) {
+        
+        var dbo = db.db("mydb") //db name here
+        if (err) throw err;
+
+        var obj = {};//references to form data and schema go here
+    
+        dbo.collection("").insertOne(myobj, function(err, res) { //same deal with the table name
+            if (err) throw err;
+            console.log("recipe added to db");
+            db.close();
+        });
+    })
 })
 
 
