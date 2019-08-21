2593a91a755ed8daf856b74cd58e65e9753e5566
Gerald Bryant <hakeem1106@gmail.com>
2019-08-20 22:32:39 -0400

    Atteming to kill add recipe page and merge

--- src/components/AddRecipeButton.js
+++ src/components/AddRecipeButton.js
@@ -4,20 +4,15 @@ import "../containers/recipecard.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { Button } from "react-bootstrap";
 
+
+
+
 const AddRecipeButton = () => {
+
   return (
     <div>
-      <Button>
-        <NavLink
-          to="/addRecipe"
-          className="add-recipe-link"
-          style={{ color: "whitesmoke", textDecoration: "none" }}
-        >
-          Add Recipe
-        </NavLink>
-      </Button>
+      
     </div>
   );
 };
-
 export default AddRecipeButton;
--- src/pages/RecipesPage.js
+++ src/pages/RecipesPage.js
@@ -1,12 +1,34 @@
 import React, { Component } from 'react';
+import {NavLink} from 'react-router-dom'
 import Navigation from '../components/Navigation';
 import RecipeCard from '../components/RecipeCard';
 import AddRecipeButton from '../components/AddRecipeButton';
 import '../containers/recipespage.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Footer from '../components/Footer';
+import RecipeForm from '../components/RecipeForm'
+import{Button} from 'react-bootstrap'
 
 class RecipesPage extends Component {
+    constructor(props){
+        super(props)
+        this.state={
+            newFood: {hidden: true}
+
+            
+        }
+        this.addNew= this.addNew.bind(this)
+    }
+    
+    addNew=(event)=>{
+        event.preventDefault()
+        this.setState({
+            
+        })
+        console.log(this.state.newFood)
+        console.log("this was pressed")
+      }
+
 
     render() {
         return (
@@ -27,7 +49,12 @@ class RecipesPage extends Component {
 
                 <div className="add-recipe">
                     <h6>Like to add your own recipe?</h6>
-                    <AddRecipeButton />
+                    <Button onClick={this.addNew}
+                    style={{ color: "whitesmoke", textDecoration: "none" }}
+                    className="add-recipe-link"
+                    >
+                     Add Recipe
+                    </Button>
                 </div>
                 <Footer />
             </div>
